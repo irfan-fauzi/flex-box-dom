65d80df62c82d268aaa91364774eb6c535c90c4c
irfan-fauzi <irfanwintech@yahoo.com>
2020-05-11 21:37:37 +0700

    check

--- evaluasi-1(looping-function-ifelse)/js/index.js
+++ evaluasi-1(looping-function-ifelse)/js/index.js
@@ -1,12 +1,12 @@
-let container2 = document.querySelector('.container2');
+// let container2 = document.querySelector('.container2');
 let container = document.querySelector('.container');
 
 
 let newEl = function (element, text) {
     let el = document.createElement(element);
     el.innerText = text;
-    el.classList.add('kotak')
-    return container2.appendChild(el);
+    el.classList.add('box')
+    return container.appendChild(el);
 }
 
 let looping = function (element, text, jumlah) {
@@ -16,15 +16,15 @@ let looping = function (element, text, jumlah) {
 }
 
 
-looping(`div`, `baru`, 5);
+looping(`div`, `baru`, 10);
 
 // event onclick
-let myDiv = container2.querySelectorAll('div');
+let myDiv = container.querySelectorAll('div');
 for (let i = 0; i < myDiv.length; i++) {
 
     for (let j = i; j <= i; j++) {
 
-        myDiv[i].classList.add(`kotak${j +1}`);
+        myDiv[i].classList.add(`box${j +1}`);
     }
 
 }
